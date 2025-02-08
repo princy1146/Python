11220b79b2a150e9353e214d9e6cd2658fb0ace0
princy1146 <princyshah1104@gmail.com>
2025-02-09 00:06:52 +0530

    Add files via upload

---
+++ princy21.py
@@ -0,0 +1,5 @@
+#Calculate net salary
+gross_salary = float(input())
+allowance = 0.1 * gross_salary
+deduction = 0.03 * gross_salary
+print(gross_salary + allowance - deduction)
---
+++ princy22.py
@@ -0,0 +1,3 @@
+#Calculate net sales
+gross_sales = float(input())
+print(gross_sales - (0.1*gross_sales))
